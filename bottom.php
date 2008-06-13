b03ec696bbf766d026414c74cc86fcef5287332a
eyerouge <eyerouge@21afd39c-894f-0410-bccb-80715ca37297>
2008-06-13 15:29:56 +0000

    killed html-tags & aligned text.

--- bottom.php
+++ bottom.php
@@ -11,7 +11,7 @@
 &nbsp;
 &nbsp;
 &nbsp;
-<p align="center" class="copyleft">the ladder is not in any way associated with the official wesnoth forum, it's moderators and/or the developers of Wesnoth.<br><a href="rss.php">rss</a> / power <a href="https://sourceforge.net/projects/gamingladder">tribal bliss</a> / <a href="https://sourceforge.net/pm/?group_id=224204">to-do</a><br>contact: eyerouge(x)eyerouge(y)com</p>
+<p align="center" class="copyleft">the ladder is not in any way associated with the official wesnoth forum, it's moderators and/or the developers of Wesnoth.<br><a href="rss.php">rss</a> / power <a href="https://sourceforge.net/projects/gamingladder">tribal bliss</a> / <a href="https://sourceforge.net/pm/?group_id=224204">to-do</a><br>contact: spam (x) eyerouge (y) com</p>
 
 </td>
 </tr>
@@ -30,12 +30,8 @@ $endarray = explode(" ", $endtime);
 $endtime = $endarray[1] + $endarray[0];
 $totaltime = $endtime - $starttime;
 $totaltime = round($totaltime, 5);
-echo "<br>Loaded in $totaltime seconds.";
+echo "<p align=\"center\" class=\"copyleft\">$totaltime sec.</div>";
 ?>
 </body>
 </html>
-</body>
-</html>
 
-</body>
-</html>
