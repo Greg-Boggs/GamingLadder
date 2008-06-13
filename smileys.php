f41b84cfb376161346a393bf9142c7a0e56c2543
mr-russ <mr-russ@21afd39c-894f-0410-bccb-80715ca37297>
2008-06-13 12:35:27 +0000

    Move the smileys folder under the graphics folder for clarity of location of items.  They are all graphics, just for a specific purpose.  They are only referenced in the smileys.php files.

--- smileys.php
+++ smileys.php
@@ -1,34 +1,33 @@
 <?
-$content = eregi_replace(quotemeta(":)"),"<img src='smileys/smile.gif' border='0'>",$content);
-$content = eregi_replace(quotemeta(":("),"<img src='//smileys/sad.gif' border='0'>",$content);
-$content = eregi_replace(quotemeta(":D"),"<img src='smileys/biggrin.gif' border='0'>",$content);
-$content = eregi_replace(quotemeta(":'("),"<img src='smileys/cry.gif' border='0'>",$content);
-$content = eregi_replace(quotemeta(":o"),"<img src='smileys/bigeek.gif' border='0'>",$content);
-$content = eregi_replace(quotemeta(";)"),"<img src='smileys/wink.gif' border='0'>",$content);
-$content = eregi_replace(quotemeta("(y)"),"<img src='smileys/yes.gif' border='0'>",$content);
-$content = eregi_replace(quotemeta("(n)"),"<img src='smileys/no.gif' border='0'>",$content);
-$content = eregi_replace(quotemeta(":p"),"<img src='smileys/bigrazz.gif' border='0'>",$content);
-$content = eregi_replace(quotemeta(":@"),"<img src='smileys/mad.gif' border='0'>",$content);
-$content = eregi_replace(quotemeta(":s"),"<img src='smileys/none.gif' border='0'>",$content);
-$content = eregi_replace(quotemeta(":x"),"<img src='smileys/dead.gif' border='0'>",$content);
-$content = eregi_replace(quotemeta(":b"),"<img src='smileys/cool.gif' border='0'>",$content);
-$content = eregi_replace(quotemeta(":h"),"<img src='smileys/laugh.gif' border='0'>",$content);
-$content = eregi_replace(quotemeta(":r"),"<img src='smileys/rolleyes.gif' border='0'>",$content);
-?>
-<?
-$news = eregi_replace(quotemeta(":)"),"<img src='smileys/smile.gif' border='0'>",$news);
-$news = eregi_replace(quotemeta(":("),"<img src='//smileys/sad.gif' border='0'>",$news);
-$news = eregi_replace(quotemeta(":D"),"<img src='smileys/biggrin.gif' border='0'>",$news);
-$news = eregi_replace(quotemeta(":'("),"<img src='smileys/cry.gif' border='0'>",$news);
-$news = eregi_replace(quotemeta(":o"),"<img src='smileys/bigeek.gif' border='0'>",$news);
-$news = eregi_replace(quotemeta(";)"),"<img src='smileys/wink.gif' border='0'>",$news);
-$news = eregi_replace(quotemeta("(y)"),"<img src='smileys/yes.gif' border='0'>",$news);
-$news = eregi_replace(quotemeta("(n)"),"<img src='smileys/no.gif' border='0'>",$news);
-$news = eregi_replace(quotemeta(":p"),"<img src='smileys/bigrazz.gif' border='0'>",$news);
-$news = eregi_replace(quotemeta(":@"),"<img src='smileys/mad.gif' border='0'>",$news);
-$news = eregi_replace(quotemeta(":s"),"<img src='smileys/none.gif' border='0'>",$news);
-$news = eregi_replace(quotemeta(":x"),"<img src='smileys/dead.gif' border='0'>",$news);
-$news = eregi_replace(quotemeta(":b"),"<img src='smileys/cool.gif' border='0'>",$news);
-$news = eregi_replace(quotemeta(":h"),"<img src='smileys/laugh.gif' border='0'>",$news);
-$news = eregi_replace(quotemeta(":r"),"<img src='smileys/rolleyes.gif' border='0'>",$news);
+$content = eregi_replace(quotemeta(":)"),"<img src='graphics/smileys/smile.gif' border='0'>",$content);
+$content = eregi_replace(quotemeta(":("),"<img src='graphics/smileys/sad.gif' border='0'>",$content);
+$content = eregi_replace(quotemeta(":D"),"<img src='graphics/smileys/biggrin.gif' border='0'>",$content);
+$content = eregi_replace(quotemeta(":'("),"<img src='graphics/smileys/cry.gif' border='0'>",$content);
+$content = eregi_replace(quotemeta(":o"),"<img src='graphics/smileys/bigeek.gif' border='0'>",$content);
+$content = eregi_replace(quotemeta(";)"),"<img src='graphics/smileys/wink.gif' border='0'>",$content);
+$content = eregi_replace(quotemeta("(y)"),"<img src='graphics/smileys/yes.gif' border='0'>",$content);
+$content = eregi_replace(quotemeta("(n)"),"<img src='graphics/smileys/no.gif' border='0'>",$content);
+$content = eregi_replace(quotemeta(":p"),"<img src='graphics/smileys/bigrazz.gif' border='0'>",$content);
+$content = eregi_replace(quotemeta(":@"),"<img src='graphics/smileys/mad.gif' border='0'>",$content);
+$content = eregi_replace(quotemeta(":s"),"<img src='graphics/smileys/none.gif' border='0'>",$content);
+$content = eregi_replace(quotemeta(":x"),"<img src='graphics/smileys/dead.gif' border='0'>",$content);
+$content = eregi_replace(quotemeta(":b"),"<img src='graphics/smileys/cool.gif' border='0'>",$content);
+$content = eregi_replace(quotemeta(":h"),"<img src='graphics/smileys/laugh.gif' border='0'>",$content);
+$content = eregi_replace(quotemeta(":r"),"<img src='graphics/smileys/rolleyes.gif' border='0'>",$content);
+
+$news = eregi_replace(quotemeta(":)"),"<img src='graphics/smileys/smile.gif' border='0'>",$news);
+$news = eregi_replace(quotemeta(":("),"<img src='graphics/smileys/sad.gif' border='0'>",$news);
+$news = eregi_replace(quotemeta(":D"),"<img src='graphics/smileys/biggrin.gif' border='0'>",$news);
+$news = eregi_replace(quotemeta(":'("),"<img src='graphics/smileys/cry.gif' border='0'>",$news);
+$news = eregi_replace(quotemeta(":o"),"<img src='graphics/smileys/bigeek.gif' border='0'>",$news);
+$news = eregi_replace(quotemeta(";)"),"<img src='graphics/smileys/wink.gif' border='0'>",$news);
+$news = eregi_replace(quotemeta("(y)"),"<img src='graphics/smileys/yes.gif' border='0'>",$news);
+$news = eregi_replace(quotemeta("(n)"),"<img src='graphics/smileys/no.gif' border='0'>",$news);
+$news = eregi_replace(quotemeta(":p"),"<img src='graphics/smileys/bigrazz.gif' border='0'>",$news);
+$news = eregi_replace(quotemeta(":@"),"<img src='graphics/smileys/mad.gif' border='0'>",$news);
+$news = eregi_replace(quotemeta(":s"),"<img src='graphics/smileys/none.gif' border='0'>",$news);
+$news = eregi_replace(quotemeta(":x"),"<img src='graphics/smileys/dead.gif' border='0'>",$news);
+$news = eregi_replace(quotemeta(":b"),"<img src='graphics/smileys/cool.gif' border='0'>",$news);
+$news = eregi_replace(quotemeta(":h"),"<img src='graphics/smileys/laugh.gif' border='0'>",$news);
+$news = eregi_replace(quotemeta(":r"),"<img src='graphics/smileys/rolleyes.gif' border='0'>",$news);
 ?>
