913e55a39f40eefd989b2b3ef1e4bb2d3621ec7a
mr-russ <mr-russ@21afd39c-894f-0410-bccb-80715ca37297>
2008-06-22 03:03:08 +0000

    Cleanup ip code to be simplier and more readable.  The SQL is more complicated.  However it now does all the work of grouping all of the users. To cleanup the interface a little everything has been placed in a table, rather than a set of paragraphs.

--- ip.php
+++ ip.php
@@ -1,61 +1,23 @@
-<?
-//session_start();
-//echo "session: ". $_SESSION['username'];
-$page = "ip";
+<?php
 require('conf/variables.php');
 require('top.php');
-
-//if ( isset($_SESSION['username']) ) {
 ?>
-<p class="header">Ip-check.</p>
-<?
-$count = 0;
-$sql="SELECT * FROM $playerstable ORDER BY ip ASC, name ASC";
-$result=mysql_query($sql,$db);
-$num = mysql_num_rows($result);
-$cur = 1;
-while ($num >= $cur) {
-$row = mysql_fetch_array($result);
-$name = $row["name"];
-$ip2 = $row["ip"];
-if ($ip2 == $ip) {
-$show = 'yes';
-$count++;
-}
-else {
-$show = 'no';
-}
-$ip = $ip2;
+<p class="header">Duplicate IP check.</p>
+<p>Identify players that are using the same IP address at the moment.</p>
+<?php
+$sql = "SELECT GROUP_CONCAT(name ORDER BY name SEPARATOR ', ') as namelist FROM $playerstable GROUP BY ip HAVING count(name) > 1 ORDER BY namelist ASC";
+$result = mysql_query($sql, $db);
 
-if ($ip != "") {
-if ($show == 'yes') {
-if ($ip != $ip3) {
-echo "<p class='text'><b>Same ip</b>:<br>";
-$sql2="SELECT * FROM $playerstable WHERE ip = '$ip'";
-$result2=mysql_query($sql2,$db);
-$num2 = mysql_num_rows($result2);
-$cur2 = 1;
-while ($num2 >= $cur2) {
-$row2 = mysql_fetch_array($result2);
-$name2 = $row2["name"];
-echo "$name2<br>";
-$cur2++;
-}
-$ip3 = $ip;
-echo"<br>";
-}
-}
-}
-$cur++;
-}
-if ($count < 1) {
+if (mysql_num_rows($result) == 0) {
 ?>
 <p class="text">There are no players with identical ip's.</p>
-<?
+<?php
+} else {
+    echo '<table width="80%" border="1" bgcolor="#E7D9C0" cellspacing="0" cellpadding="2">';
+    while ($row = mysql_fetch_array($result)) {
+        echo "<tr><td class='text'>".$row['namelist']."</td></tr>";
+    }
+	echo "</table>";
 }
-/*}else{
-echo "<p class='header'>You are not allowed to view this part of the site.<br><br>
-<p class='text'><a href='../index.php'><font color='$color1'>Login.</font></a></p>";
-}*/
 require('bottom.php');
 ?>
