f28bb529ca997d4039a91d4e122f4d5147ee572e
mr-russ <mr-russ@21afd39c-894f-0410-bccb-80715ca37297>
2008-07-12 04:09:18 +0000

    Add search options to the game history.  creates a much more usable interface for game searching.

--- gamehistory.php
+++ gamehistory.php
@@ -2,6 +2,30 @@
 session_start();
 // v 1.01
 require('conf/variables.php');
+
+// Handle all of the cookie management before we attempt to do anything else
+$searchArray = unserialize($_COOKIE['gamehistoryoptions']);
+if (isset($_GET['reporteddirection'])) $searchArray['reporteddirection'] = $_GET['reporteddirection'];
+if (isset($_GET['reportdate'])) $searchArray['reportdate'] = $_GET['reportdate'];
+if (isset($_GET['reportdate'])) $searchArray['reportdate'] = $_GET['reportdate'];
+if (isset($_GET['winner'])) $searchArray['winner'] = $_GET['winner'];
+if (isset($_GET['loser'])) $searchArray['loser'] = $_GET['loser'];
+if (isset($_GET['loserratingdirection'])) $searchArray['loserratingdirection'] = $_GET['loserratingdirection'];
+if (isset($_GET['winnerratingdirection'])) $searchArray['winnerratingdirection'] = $_GET['winnerratingdirection'];
+if (isset($_GET['winnerrating'])) $searchArray['winnerrating'] = $_GET['winnerrating'];
+if (isset($_GET['loserrating'])) $searchArray['loserrating'] = $_GET['loserrating'];
+if (isset($_GET['replay'])) $searchArray['replay'] = $_GET['replay'];
+if (isset($_GET['playerand'])) $searchArray['playerand'] = $_GET['playerand'];
+if (isset($_GET['ratingand'])) $searchArray['ratingand'] = $_GET['ratingand'];
+
+if ($searchArray['reporteddirection'] <> "<=" && $searchArray['reporteddirection'] <> ">=") $searchArray['reporteddirection'] = "";
+if ($searchArray['winnerratingdirection'] <> "<=" && $searchArray['winnerratingdirection'] <> ">=") $searchArray['winnerratingdirection'] = "";
+if ($searchArray['loserratingdirection'] <> "<=" && $searchArray['loserratingdirection'] <> ">=") $searchArray['loserratingdirection'] = "";
+if ($searchArray['playerand'] <> "AND" && $searchArray['playerand'] <> "OR") $searchArray['playerand'] = "AND";
+if ($searchArray['ratingand'] <> "AND" && $searchArray['ratingand'] <> "OR") $searchArray['ratingand'] = "AND";
+
+setcookie ("gamehistoryoptions", serialize($searchArray), time()+7776000); 
+
 require_once 'autologin.inc.php';
 require('top.php');
 
@@ -12,63 +36,9 @@ if (isset($_REQUEST['selectname'])) {
 }
 ?>
 <p class="header">Played games.</p>
-<form method="post" action="gamehistory.php?startplayed=0&finishplayed=<?echo"$numgamespage"?>">
-<p class="text"><a href="gamehistory.php?startplayed=0&finishplayed=<?echo"$numgamespage"?>"><font color="<?echo"$color1"?>">View all games</font></a> |
-View games from: <select size="1" name="selectname" style="background-color: <?echo"$color5" ?>; border: 1 solid <?echo"$color1" ?>" class="text">
-<?
-$sortby = "name ASC";
-$sql = "SELECT name FROM $playerstable ORDER BY $sortby";
-$result = mysql_query($sql,$db);
-
-while ($row = mysql_fetch_array($result)) {
-    if ($_REQUEST[selectname] == $row[name]) {
-        echo"<option selected>$row[name]</option>";
-    } else {
-        echo"<option>$row[name]</option>";
-    }
-}
-?>
-</select> <input type="Submit" name="submit2" value="View." style="background-color: <?echo"$color5" ?>; border: 1 solid <?echo"$color1" ?>" class="text">
-</form>
-<?
-if (isset($_REQUEST['selectname'])) {
-    $where = "WHERE winner = '".$_REQUEST['selectname']."' OR loser = '".$_REQUEST['selectname']."' ";
-} else {
-    $where = "";
-}
-$sql="SELECT count(*) FROM $gamestable ".$where;
-$result = mysql_query($sql, $db);
-$row = mysql_fetch_row($result);
-$yo = $row[0];
-
-$number = 0;
-$link = 1;
-$finishnumber = $numgamespage;
-$startnext = $_GET[startplayed] + $numgamespage;
-$startprevious = $_GET[startplayed] - $numgamespage;
-echo "<p class='text'>Go to page:";
-if ($startprevious >= 0) {
-    echo "&nbsp;|&nbsp;<a href='gamehistory.php?startplayed=$startprevious&finishplayed=$finishnumber".$playerquerystring."'><font color='$color1'><</font></a>&nbsp;|";
-}
-while ($number < $yo) {
-    echo "&nbsp;<a href='gamehistory.php?startplayed=$number&finishplayed=$finishnumber".$playerquerystring."'><font color='$color1'>$link</font></a>&nbsp;|&nbsp;";
-    $number = $number + $numgamespage;
-    $link = $link + 1;
-}
-if ($startplayed < $yo - $numgamespage) {
-    echo "<a href='gamehistory.php?startplayed=$startnext&finishplayed=$finishnumber".$playerquerystring."'><font color='$color1'>></font></a>&nbsp;|";
-}
-
-if ($approvegames == "yes") {
-    $tablewidth = "80%";
-    $width = "25%";
-} else {
-    $width = "33%";
-    $tablewidth = "60%";
-}
-?>
-<br /><br />
-
+<p>A maximum of 250 games will be displayed at any one time.  If games are missing, please refine your search further. They are selected
+in reported on order descending, so any missing games will be the oldest games from the selection criteria.</p>
+<form method="get" action="gamehistory.php">
 <script type="text/javascript">
 $(document).ready(function() 
     { 
@@ -77,7 +47,7 @@ $(document).ready(function()
 ); 
 </script>
 
-<table width="<?echo"$tablewidth" ?>" id="games" class="tablesorter">
+<table width="60%" id="games" class="tablesorter">
 <thead>
 <tr>
 <th>Reported Time</th>
@@ -86,23 +56,84 @@ $(document).ready(function()
 <th>Winner New Rating</th>
 <th>Loser New Rating</th>
 <th>Replay</th>
-<?
-if ($approvegames == "yes") {
-?>
-<th>Status</th>
-<?
-}
-?>
+</tr>
+<tr>
+<td><select name="reporteddirection">
+    <option <?php if ($searchArray['reporteddirection'] == "") echo "selected='selected'"; ?> value="">--</option>
+    <option <?php if ($searchArray['reporteddirection'] == "<=") echo "selected='selected'"; ?> value="&lt;=">&lt;=</option>
+    <option <?php if ($searchArray['reporteddirection'] == ">=") echo "selected='selected'"; ?> value="&gt;=">&gt;=</option>
+    </select>
+    <input type="text" value="<?php echo $searchArray['reportdate']; ?>" name="reportdate" size="9" /></td>
+<td><input type="text" value="<?php echo $searchArray['winner']; ?>" name="winner" size="10" /></td>
+<td><input type="text" value="<?php echo $searchArray['loser']; ?>" name="loser" size="10" /></td>
+<td><select name="winnerratingdirection">
+    <option <?php if ($searchArray['winnerratingdirection'] == "") echo "selected='selected'"; ?> value="">--</option>
+    <option <?php if ($searchArray['winnerratingdirection'] == "<=") echo "selected='selected'"; ?> value="&lt;=">&lt;=</option>
+    <option <?php if ($searchArray['winnerratingdirection'] == ">=") echo "selected='selected'"; ?> value="&gt;=">&gt;=</option>
+    </select>
+    <input type="text" value="<?php echo $searchArray['winnerrating']; ?>" name="winnerrating" size="4" />
+</td>
+<td><select name="loserratingdirection">
+    <option <?php if ($searchArray['loserratingdirection'] == "") echo "selected='selected'"; ?> value="">--</option>
+    <option <?php if ($searchArray['loserratingdirection'] == "<=") echo "selected='selected'"; ?> value="&lt;=">&lt;=</option>
+    <option <?php if ($searchArray['loserratingdirection'] == ">=") echo "selected='selected'"; ?> value="&gt;=">&gt;=</option>
+    </select>
+    <input type="text" value="<?php echo $searchArray['loserrating']; ?>" name="loserrating" size="4" />
+</td>
+<td><select name="replay">
+    <option <?php if ($searchArray['replay'] == "") echo "selected='selected'"; ?> value="">All</option>
+    <option <?php if ($searchArray['replay'] == "yes") echo "selected='selected'"; ?> value="yes">Yes</option>
+    <option <?php if ($searchArray['replay'] == "no") echo "selected='selected'"; ?> value="no">No</option>
+    </select>
+</td>
+</tr>
+<tr>
+<td style="vertical-align: middle; text-align: center"><input type="submit" name="submit" value="Update Search" /></td>
+<td style="vertical-align: middle; text-align: center" colspan="2"><select name="playerand">
+   <option <?php if ($searchArray['playerand'] == "AND") echo "selected='selected'"; ?> value="AND">and</option>
+   <option <?php if ($searchArray['playerand'] == "OR") echo "selected='selected'"; ?> value="OR">or</option>
+   </select> above options together.</td>
+<td style="vertical-align: middle; text-align: center" colspan="2"><select name="ratingand">
+   <option <?php if ($searchArray['ratingand'] == "AND") echo "selected='selected'"; ?> value="AND">and</option>
+   <option <?php if ($searchArray['ratingand'] == "OR") echo "selected='selected'"; ?> value="OR">or</option>
+   </select> above options together.</td>
+<td>&nbsp;</td>
 </tr>
 </thead>
 <tbody>
-<?
-if ($_REQUEST[selectname]) {
-    $sql = "SELECT withdrawn, contested_by_loser, DATE_FORMAT(reported_on, '".$GLOBALS['displayDateFormat']."') as report_time, reported_on, winner, loser, winner_points, loser_points, winner_elo, loser_elo, length(replay) as is_replay, replay_downloads FROM $gamestable WHERE winner = '$_REQUEST[selectname]' OR loser = '$_REQUEST[selectname]'  ORDER BY reported_on DESC LIMIT $_GET[startplayed], $_GET[finishplayed]";
-} else {
-    $sql = "SELECT withdrawn, contested_by_loser, DATE_FORMAT(reported_on, '".$GLOBALS['displayDateFormat']."') as report_time, reported_on, winner, loser, winner_points, loser_points, winner_elo, loser_elo, length(replay) as is_replay, replay_downloads FROM $gamestable ORDER BY reported_on DESC LIMIT $_GET[startplayed], $_GET[finishplayed]";
+<?php
+// Select the games we are interested in
+
+// Build where clause
+// Store all this in a cookie
+$where = "(winner like '%".$searchArray['winner']."%' ".$searchArray['playerand']." loser like '%".$searchArray['loser']."%') ";
+
+// Setup ratings in query
+if ($searchArray['winnerratingdirection'] != "") {
+    $where .= " AND (winner_elo ".$searchArray['winnerratingdirection']." '".$searchArray['winnerrating']."' ".$searchArray['ratingand']." ";
+    if ($searchArray['loserratingdirection'] != "") {
+        $where .= " loser_elo ".$searchArray['loserratingdirection']." '".$searchArray['loserrating']."') ";
+    } else {
+        $where .= " 1=1)";
+    }
 }
-$result = mysql_query($sql,$db);
+
+// Add replay restrictions
+if ($searchArray['replay'] == "no") {
+    $where .= " AND length(replay) IS NULL ";
+} else if ($searchArray['replay'] == "yes") {
+    $where .= " AND length(replay) > 0 ";
+}
+
+// Add reported_on restrictions
+if ($searchArray['reporteddirection'] != "") {
+    $where .= " AND reported_on ".$searchArray['reporteddirection']." '".$searchArray['reportdate']."' ";
+}
+
+// Build the select
+$sql = "SELECT withdrawn, contested_by_loser, DATE_FORMAT(reported_on, '".$GLOBALS['displayDateFormat']."') as report_time, reported_on, winner, loser, winner_points, loser_points, winner_elo, loser_elo, length(replay) as is_replay, replay_downloads FROM $gamestable WHERE $where ORDER BY reported_on DESC LIMIT 250";
+
+$result = mysql_query($sql, $db);
 while ($row = mysql_fetch_array($result)) {
     if ($row["recorded"] == "yes") {
         $status = "recorded";
@@ -147,19 +178,13 @@ while ($row = mysql_fetch_array($result)) {
     }
 ?>
 </td>
-<?php
-    if ($approvegames == "yes") {
-?>
-<td><?echo $sdel."$status".$edel ?></td>
-<?
-    }
-?>
 </tr>
 <?
 }
 ?>
 </tbody>
 </table>
+</form>
 
 <br />
 <?
