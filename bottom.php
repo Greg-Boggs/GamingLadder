0345fe3ff1b095392d2b92f38b269cf484537f3d
mr-russ <mr-russ@21afd39c-894f-0410-bccb-80715ca37297>
2008-07-13 03:33:16 +0000

    Add version number to bottom.php

--- bottom.php
+++ bottom.php
@@ -1,5 +1,6 @@
 <p align="center" class="copyleft">the ladder is not in any way associated with the official wesnoth forum, it's moderators and/or the developers of Wesnoth.
-<br /><a href="<?php echo $GLOBALS['prefix'] ?>ip.php">dupe check</a> / <a href="<?php echo $GLOBALS['prefix'] ?>rss.php">rss</a> / power <a href="https://sourceforge.net/projects/gamingladder">tribal bliss</a> / <a href="https://sourceforge.net/pm/?group_id=224204">to-do</a><br />contact: spam (x) eyerouge (y) com</p>
+<br /><a href="<?php echo $GLOBALS['prefix'] ?>ip.php">dupe check</a> / <a href="<?php echo $GLOBALS['prefix'] ?>rss.php">rss</a> / powered by <a href="https://sourceforge.net/projects/gamingladder">tribal bliss</a> v0.5.0 / <a href="https://sourceforge.net/pm/?group_id=224204">to-do</a>
+<br />contact: spam (x) eyerouge (y) com</p>
 
 <?php
 // Admins can impersonate other users on the ladder, it's important to display the fact they are behaving like another user at
