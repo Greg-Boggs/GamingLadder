cf101ee5e4022a23a3eb83b61995f4f33176e9d2
eyerouge <eyerouge@21afd39c-894f-0410-bccb-80715ca37297>
2009-09-16 20:45:46 +0000

    // Fixed
    - tesafilmchen discovered a bug in the configfile that hindered the cache table from being created, which in turn hindered the ladder from being populated with rankings etc.

--- report.php
+++ report.php
@@ -261,8 +261,8 @@ if (isset($_POST['report'])) {
 	echo "<p>Thank you! Information entered. Check your <a href=\"ladder.php?personalladder=".urlencode($_SESSION['username'])."\">current position.</a><br />Report Id: ". $cloneresult['reportedTime'] . " | " . $winner." / ". $loser ."</p>";
     
   // So the report was done and all that the player entered put into the db. Finally we recache the ladder, it takes about 1-2 seconds with 25000 games
-        mysql_query("TRUNCATE TABLE $standingscachetable", $db);	
-        mysql_query("INSERT INTO $standingscachetable ".$cacheSql, $db);	
+        mysql_query("TRUNCATE TABLE $standingscachetable", $db) or die(mysql_error());
+        mysql_query("INSERT INTO $standingscachetable ".$cacheSql, $db) or die(mysql_error());
 		require_once 'include/morecachestandings.inc.php';   
 		
 
