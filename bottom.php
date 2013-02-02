027d2a0dec3dd72f197e5c19bdc527f1f7ff677a
gboggs <gboggs@21afd39c-894f-0410-bccb-80715ca37297>
2013-02-02 20:38:59 +0000

    login is now blockable by admin

--- bottom.php
+++ bottom.php
@@ -4,9 +4,9 @@ if (isset($cronbottommsg)){
 echo "<b>$cronbottommsg</b><br>"; 
 }
 ?>the ladder is not in any way associated with the official wesnoth forum, it's moderators and/or the developers of Wesnoth.
-<br /><a href="<?php echo $GLOBALS['prefix'] ?>ip.php">dupe check</a> / <a href="<?php echo $GLOBALS['prefix'] ?>rss.php">rss</a> / powered by <a href="https://sourceforge.net/projects/gamingladder/?abmode=1">Gaming Ladder</a> <?php echo TRIBAL_VERSION ?> / <a href="https://sourceforge.net/pm/?group_id=224204">to-do</a>
+<br /><a href="<?php echo $GLOBALS['prefix'] ?>ip.php">dupe check</a> / <a href="<?php echo $GLOBALS['prefix'] ?>rss.php">rss</a> / powered by <a href="https://sourceforge.net/projects/gamingladder/?abmode=1">Gaming Ladder</a> <?php echo TRIBAL_VERSION ?> / <a href="https://sourceforge.net/pm/?group_id=224204">to-do</a><br />
+Maintained with care by <a href="http://www.gregboggs.com">Drupal Developer</a> Greg Boggs.<br />
 <?php echo "<br />contact: ". FOOTER_MAIL ?>
-<p>Maintained with care by <a href="http://www.gregboggs.com">Drupal Developer</a> Greg Boggs.</p>
 </p>
 
 <?php
--- logincheck.inc.php
+++ logincheck.inc.php
@@ -1,11 +1,9 @@
-edi<?php
+<?php
 require('conf/variables.php');
 
 // Check if the user is logged in using the session, if not display the login screen for them and quit.
 if (!isset($_SESSION['real-username'])) {
     require('top.php');
-
-
     echo "<h1>Access denied.</h1><br><p>Please <a href=\"index.php\">log in</a> to use this function. " .
     "Only members of the ladder can access this page. <a href=\"join.php\">Become one</a> and compete today!</p>";
     require('bottom.php');
